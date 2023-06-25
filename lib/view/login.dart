3312d86d5c129077618b0ba181ec41d1b27358f5
Zahran Rafif <zahranlumia@gmail.com>
2023-06-26 01:55:24 +0700

    elevated button login berhasil

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:rentbike/controller/auth_controller.dart';
+import 'package:rentbike/model/user_model.dart';
+import 'package:rentbike/view/homepage.dart';
 
 class Login extends StatelessWidget {
   final formkey = GlobalKey<FormState>();
 
-  final autcr = AuthController();
+  final autCtr = AuthController();
   String? email;
   String? password;
 
@@ -41,6 +43,41 @@ class Login extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
+                ElevatedButton(
+                    child: Text('Login'),
+                    onPressed: () async {
+                      if (formkey.currentState!.validate()) {
+                        UserModel? signUser = await autCtr
+                            .signWithEmailAndPassword(email!, password!);
+
+                        if (signUser != null) {
+                          // Registration successful
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Login Berhasil'),
+                                content: const Text('Kamu Berhasil Login'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  HomePage()));
+                                      print(signUser.name);
+                                      // Navigate to the next screen or perform any desired action
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      }
+                    })
               ],
             ),
           )),
