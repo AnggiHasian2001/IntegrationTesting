1407e6e262ad71c544a366f64ed4a81aa67ac408
Zahran Rafif <zahranlumia@gmail.com>
2023-06-26 01:42:44 +0700

    form login email dan password

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel {
--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,11 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rentbike/controller/auth_controller.dart';
 
 class Login extends StatelessWidget {
-  const Login({super.key});
+  final formkey = GlobalKey<FormState>();
+
+  final autcr = AuthController();
+  String? email;
+  String? password;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Form(
+            key: formkey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Email"),
+                  onChanged: (value) {
+                    email = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Password"),
+                  onChanged: (value) {
+                    password = value;
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
