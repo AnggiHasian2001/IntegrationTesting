2211218cda68678cba65d7d420f59e46ee1024f6
Zahran Rafif <zahranlumia@gmail.com>
2023-06-25 15:28:54 +0700

    tampilan view register

--- lib/view/register.dart
+++ lib/view/register.dart
@@ -1,16 +1,162 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rentbike/view/login.dart';
+import '../model/user_model.dart';
 
-class Register extends StatefulWidget {
-  const Register({super.key});
+class Register extends StatelessWidget {
+  final formkey = GlobalKey<FormState>();
+  final autCtr = AuthController();
 
-  @override
-  State<Register> createState() => _RegisterState();
-}
+  String? name;
+  String? email;
+  String? password;
 
-class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please type an name';
+                  }
+                },
+                decoration: InputDecoration(hintText: "Name"),
+                onChanged: (value) {
+                  name = value;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please type an email';
+                  }
+                },
+                decoration: InputDecoration(hintText: "Email"),
+                onChanged: (value) {
+                  email = value;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.length < 6) {
+                    return 'Your password needs to be at least 6 characters';
+                  }
+                },
+                decoration: InputDecoration(hintText: "Password"),
+                onChanged: (value) {
+                  password = value;
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                child: Text('Register'),
+                onPressed: () async {
+                  if (formkey.currentState!.validate()) {
+                    UserModel? registeredUser = await autCtr
+                        .registerWithEmailAndPassword(email!, password!, name!);
+
+                    if (registeredUser != null) {
+                      // Registration successful
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Registration Successful'),
+                            content: const Text(
+                                'You have been successfully registered.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Login()));
+                                  print(registeredUser.name);
+                                  // Navigate to the next screen or perform any desired action
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      // Registration failed
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Registration Failed'),
+                            content: const Text(
+                                'An error occurred during registration.'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Register()));
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  }
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Login()),
+                      );
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
