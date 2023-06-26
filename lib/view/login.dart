32a030ea654a5bb7015163f60635d140c5266f2b
Zahran Rafif <zahranlumia@gmail.com>
2023-06-27 01:25:49 +0700

    mempercantik tampilan login

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -6,145 +6,213 @@ import 'package:rentbike/model/user_model.dart';
 import 'package:rentbike/view/homepage.dart';
 import 'package:rentbike/view/register.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   final formkey = GlobalKey<FormState>();
 
   final autCtr = AuthController();
+
   String? email;
+
   String? password;
+
+  bool eyeToggle = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Form(
-            key: formkey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Harap masukan Email';
-                    }
-                  },
-                  decoration: InputDecoration(hintText: "Email"),
-                  onChanged: (value) {
-                    email = value;
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (value) {
-                    if (value == null || value.length < 6) {
-                      return 'Harap masukan password setidaknya 6 karakter';
-                    }
-                  },
-                  decoration: InputDecoration(hintText: "Password"),
-                  onChanged: (value) {
-                    password = value;
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(
-                  child: Text('Login'),
-                  onPressed: () async {
-                    if (formkey.currentState!.validate()) {
-                      UserModel? signUser = await autCtr
-                          .signWithEmailAndPassword(email!, password!);
-
-                      if (signUser != null) {
-                        // Registration successful
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Login Berhasil'),
-                              content: const Text('Kamu Berhasil Login'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => HomePage()));
-                                    print(signUser.name);
-                                    // Navigate to the next screen or perform any desired action
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      } else {
-                        // Registration failed
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Login Gagal'),
-                              content:
-                                  const Text('Terdapat error saat registrasi'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => Login()));
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      }
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Form(
+                key: formkey,
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "Don't Have an Account?",
+                      'Login',
                       style: TextStyle(
-                        fontSize: 14,
+                          fontFamily: 'Raleway',
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(30),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.black,
+                        radius: 90,
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Register()),
-                        );
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Harap masukan Email';
+                        }
                       },
-                      child: Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          color: Colors.amber,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                        hintText: "Email",
+                        labelText: 'Email',
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                      ),
+                      onChanged: (value) {
+                        email = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      obscureText: eyeToggle,
+                      validator: (value) {
+                        if (value == null || value.length < 6) {
+                          return 'Harap masukan password setidaknya 6 karakter';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          contentPadding:
+                              EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                          labelText: 'Password',
+                          suffix: InkWell(
+                            onTap: () {
+                              setState(() {
+                                eyeToggle = !eyeToggle;
+                              });
+                            },
+                            child: Icon(eyeToggle
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                          )),
+                      onChanged: (value) {
+                        password = value;
+                      },
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InkWell(
+                      onTap: () async {
+                        if (formkey.currentState!.validate()) {
+                          UserModel? signUser = await autCtr
+                              .signWithEmailAndPassword(email!, password!);
+
+                          if (signUser != null) {
+                            // Registration successful
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Login Berhasil'),
+                                  content: const Text('Kamu Berhasil Login'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomePage()));
+                                        print(signUser.name);
+                                        // Navigate to the next screen or perform any desired action
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } else {
+                            // Registration failed
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text('Login Gagal'),
+                                  content: const Text(
+                                      'Terdapat error saat registrasi'),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => Login()));
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        }
+                      },
+                      child: Container(
+                        width: 500,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: Center(
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
                         ),
                       ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't Have an Account?",
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Register()),
+                            );
+                          },
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
-          )),
+                ),
+              )),
+        ),
+      ),
     );
   }
 }
