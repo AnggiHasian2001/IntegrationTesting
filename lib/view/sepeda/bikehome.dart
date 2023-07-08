247cb7316c682e61e18bd2bef2dad1eed6d1b0d2
Zahran Rafif <zahranlumia@gmail.com>
2023-07-09 02:33:56 +0700

    menampilkan gambar sepeda pada circle avatar card

--- lib/view/sepeda/bikehome.dart
+++ lib/view/sepeda/bikehome.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:gap/gap.dart';
 import 'package:rentbike/model/sepeda_model.dart';
 import 'package:rentbike/view/sepeda/add_sepeda.dart';
 import 'package:rentbike/view/sepeda/update_sepeda.dart';
@@ -14,62 +15,66 @@ class BikeHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey.shade200,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          elevation: 0,
-          title: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.amber.shade200,
-              radius: 25,
-              child: Image.asset('assets/profile.png'),
-            ),
-            title: Text(
-              'Admin',
-              style: TextStyle(fontSize: 12, color: Colors.grey.shade600),
-            ),
-            subtitle: Text(
-              'Zahran Rafif',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-            ),
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.amber.shade200,
+            radius: 25,
+            child: Image.asset('assets/profile.png'),
+          ),
+          title: Text(
+            'Admin',
+            style: TextStyle(fontSize: 12, color: Colors.grey.shade600),
+          ),
+          subtitle: Text(
+            'Zahran Rafif',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
           ),
         ),
-        body: FutureBuilder<QuerySnapshot>(
-          future: _reference.get(),
-          builder: (context, snapshot) {
-            //check apakah ada error
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('Ada masalah'),
-              );
-            }
-            //jika data berhasil diterima
-            if (snapshot.hasData) {
-              QuerySnapshot querySnapshot = snapshot.data!;
-              List<QueryDocumentSnapshot> documents = querySnapshot.docs;
-              //konversi data ke list
-              List<Sepeda> sepedas = documents
-                  .map((e) =>
-                      Sepeda(id: e['id'], name: e['name'], nomor: e['nomor']))
-                  .toList();
-              return _getBody(sepedas);
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-          // child: _getBody()
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => AddSepeda()));
-          },
-          child: const Icon(Icons.add),
-        ));
+      ),
+      body: FutureBuilder<QuerySnapshot>(
+        future: _reference.get(),
+        builder: (context, snapshot) {
+          //check apakah ada error
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Ada masalah'),
+            );
+          }
+
+          //jika data berhasil diterima
+          if (snapshot.hasData) {
+            QuerySnapshot querySnapshot = snapshot.data!;
+            List<QueryDocumentSnapshot> documents = querySnapshot.docs;
+            //konversi data ke list
+            List<Sepeda> sepedas = documents
+                .map((e) => Sepeda(
+                    id: e['id'],
+                    name: e['name'],
+                    nomor: e['nomor'],
+                    imageUrl: e['imageUrl']))
+                .toList();
+
+            return _getBody(sepedas);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddSepeda()));
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 
   Widget _getBody(sepedas) {
@@ -83,16 +88,36 @@ class BikeHome extends StatelessWidget {
         : ListView.builder(
             itemCount: sepedas.length,
             itemBuilder: (context, index) => Card(
+              elevation: 2, //menambah elevasi pada card
+              margin: EdgeInsets.symmetric(
+                  horizontal: 0, vertical: 10), //menambahkan margin card
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                      8)), //mengubah border menjadi rounded
               child: ListTile(
                 //tampil nama sepeda
-                title: Text(sepedas[index].name),
+                title: Text(
+                  sepedas[index].name,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+
                 //tampil nomor sepeda
-                subtitle: Text('Nomor : ${sepedas[index].nomor}'),
+                subtitle: Text(
+                  'Nomor : ${sepedas[index].nomor}',
+                  style: TextStyle(
+                      color: Colors.amber,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
                 //lingkarang foto
                 leading: CircleAvatar(
-                  radius: 30,
-                  child: Text('${sepedas[index].nomor}'),
-                ),
+                    radius: 30,
+                    backgroundColor: Colors.amber.shade200,
+                    foregroundColor: Colors.black,
+                    backgroundImage: NetworkImage(sepedas[index].imageUrl)),
                 trailing: SizedBox(
                     width: 60,
                     child: Row(
@@ -108,7 +133,10 @@ class BikeHome extends StatelessWidget {
                           },
                         ),
                         InkWell(
-                          child: const Icon(Icons.delete),
+                          child: const Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          ),
                           onTap: () {
                             _reference.doc(sepedas[index].id).delete();
                             //to refresh
